ed95a9509f46fe09c64fc215b124447ef82c3a96
Frolov <vfrolov@vfrolov-mbp112.local>
2016-07-20 11:00:06 -0700

    implemented vertical sliding in a carrouseel form

--- js/custom.js
+++ js/custom.js
@@ -2,53 +2,78 @@
 
     var animation_speed = 800;
     var number_of_horizontal_slides = $("span.horizontal-section").length;
-    var number_of_vertical_slides = $("#vertical-section").length;
+    var number_of_vertical_slides = $("span.vertical-section").length;
+
     var horizontal_shifts = 0;
+    var vertical_shifts = -1;
     var total_horizontal_movement = 0;
+    var total_vertical_movement = 0;
 
 
-    $( "#right" ).click( function() {
+    $("#right").click(function() {
         var gallery_width = $("#gallery").width();
         horizontal_shifts += 1;
         
-        if ( horizontal_shifts === number_of_horizontal_slides ) {
-            $( "span.horizontal-section" ).animate( { 
+        if (horizontal_shifts === number_of_horizontal_slides) {
+            $("span.horizontal-section").animate({ 
                 "left": "+=" + (total_horizontal_movement) + "px"
-            }, animation_speed );
+            }, animation_speed);
             horizontal_shifts = 0;
             total_horizontal_movement = 0;
         } else {
             total_horizontal_movement += gallery_width;
-            $( "span.horizontal-section" ).animate( { "left": "-=" + gallery_width + "px"}, animation_speed );
+            $("span.horizontal-section").animate({ "left": "-=" + gallery_width + "px"}, animation_speed);
         }
     });
+   
  
-    $( "#left" ).click( function() {
+    $("#left").click(function() {
         var gallery_width = $("#gallery").width();
         horizontal_shifts -= 1;
 
-        if ( horizontal_shifts < 0 ) {
-            $( "span.horizontal-section" ).animate( { 
+        if (horizontal_shifts < 0) {
+            $("span.horizontal-section").animate({ 
                 "left": "-=" + (gallery_width * number_of_horizontal_slides - gallery_width) + "px"
-            }, animation_speed );
+            }, animation_speed);
             horizontal_shifts = number_of_horizontal_slides - 1;
             total_horizontal_movement = gallery_width * number_of_horizontal_slides - gallery_width;
-            console.log(horizontal_shifts)
         } else {
             total_horizontal_movement -= gallery_width;
-            $( "span.horizontal-section" ).animate( { "left": "+=" + gallery_width + "px" }, animation_speed );
+            $("span.horizontal-section").animate({ "left": "+=" + gallery_width + "px" }, animation_speed);
         }
     });
 
 
-    $( "#up" ).click( function() {
+    $("#up").click(function() {
         var gallery_height = $("#gallery").height();
-        $( "span.vertical-section" ).animate( { "top": "-=" + gallery_height + "px" }, animation_speed );
-    });
+        vertical_shifts -= 1;
+        if (vertical_shifts < -1) {
+            $("span.vertical-section").animate({ 
+                "top": "-=" + (gallery_height * number_of_vertical_slides) + "px"
+            }, animation_speed);
+            vertical_shifts = number_of_vertical_slides - 1;
+            total_vertical_movement = gallery_height * number_of_vertical_slides;
+        } else {
+            total_vertical_movement -= gallery_height;
+            $("span.vertical-section").animate({"top": "+=" + gallery_height + "px"}, animation_speed);
 
-    $( "#down" ).click( function() {
+        }        
+
+    });    
+
+    $("#down").click(function() {
         var gallery_height = $("#gallery").height();
-        $( "span.vertical-section" ).animate( { "top": "+=" + gallery_height + "px" }, animation_speed );
+        vertical_shifts += 1;
+        if (vertical_shifts === number_of_vertical_slides) {
+            $("span.vertical-section").animate({ 
+                "top": "-=" + (- total_vertical_movement) + "px"
+            }, animation_speed);
+            vertical_shifts = -1;
+            total_vertical_movement = 0;
+        } else {
+            total_vertical_movement += gallery_height;
+            $("span.vertical-section").animate({"top": "-=" + gallery_height + "px"}, animation_speed);
+        }
     });        
 
 })();
